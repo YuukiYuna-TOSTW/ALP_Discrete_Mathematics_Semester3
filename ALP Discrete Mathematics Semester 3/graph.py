7039e783401546e5cd388a47c7947ac8e1d55369
Aristo Benedict Iskandar <iskandararisto@gmail.com>
2025-12-09 09:59:19 +0800

    Function Study Case

--- ALP Discrete Mathematics Semester 3/graph.py
+++ ALP Discrete Mathematics Semester 3/graph.py
@@ -3,3 +3,9 @@ import networkx as nx
 class graph:
     def __init__(self):
         self.G = nx.Graph()
+
+    def add_node(self, n):
+        self.G.add_node(n)
+
+    def add_edge(self, u, v):
+        self.G.add_edge(u, v)
--- ALP Discrete Mathematics Semester 3/main.py
+++ ALP Discrete Mathematics Semester 3/main.py
@@ -1,5 +1,2 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
-if __name__ == "__main__":
-    self.G = nx.Graph()
